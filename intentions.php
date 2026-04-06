71fc3a4fb193ba6ef65e94daed39ab91c0c51dd3
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2026-04-06 11:08:22 +0100

    2026-04-06

--- intentions.php
+++ intentions.php
@@ -125,7 +125,7 @@
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.00</td>
+            <td class="center">7.30</td>
             <td>
 + Stanisława Sapielaka
             </td>
