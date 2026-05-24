6fc28a7f853d165d5722c28fb4556a52943b1a64
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2026-05-24 06:14:37 +0200

    2026-05-24

--- archiwum15.php
+++ archiwum15.php
@@ -19,7 +19,242 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2026 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 17.&nbsp;maja 2026&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 24.&nbsp;maja 2026&nbsp;r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+            18.05 – 24.05.2026&nbsp;r.
+        </h3>
+    </div>
+    <div class="col">
+<table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+    <tbody>
+        <tr class="aktualnosci_day">
+            <td><b>PONIEDZIAŁEK, 18&nbsp;maja</b></td>
+            <td><b>Poniedziałek 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Marka Ziębę<br>
+2. O&nbsp;szczęśliwą operację i&nbsp;zdrowie dla Agnieszki
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + ks.&nbsp;Stanisława Chmielewskiego<br>
+2. + Krzysztofa (rocz.), Elżbietę i&nbsp;Kazimierza Bajdów
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>WTOREK, 19&nbsp;maja</b></td>
+            <td><b>Wtorek 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Zofię Skorupską<br>
+2. + Mieczysława Frydla
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, opiekę NMP, potrzebne łaski dla
+    Rozalii i&nbsp;Antosia w&nbsp;4&nbsp;rocznicę urodzin<br>
+2. + ks.&nbsp;Stanisława Chmielewskiego
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>ŚRODA, 20&nbsp;maja</b></td>
+            <td><b>Środa 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo dla księży Łukasza i&nbsp;Szczepana
+    w&nbsp;rocznicę święceń<br>
+2. + Mariana Cąbrowskiego
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + ks.&nbsp;Stanisława Chmielewskiego<br>
+2. + Zofię, Stanisława Kilian i&nbsp;zmarłych z&nbsp;rodziny<br>
+3. + Adama Sochę
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>CZWARTEK, 21&nbsp;maja</b></td>
+            <td><b>Czwartek 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Marka Steca<br>
+2. + Kazimierza Woźniaka
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Ewę Marciniak<br>
+2. + Wacława Boryckiego<br>
+3. + Adama Sochę<br>
+4. + Andrzeja Wosia
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>PIĄTEK, 22&nbsp;maja</b></td>
+            <td><b>Piątek 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Kazimierza Woźniaka<br>
+2. + Seweryna Rybę, Tadeusza Mazura i&nbsp;Witolda Rybę (8&nbsp;rocz. śm.)
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Jana Gajdę<br>
+2. + Krystynę Kuśmirę<br>
+3. + Adama Sochę
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>SOBOTA, 23&nbsp;maja</b></td>
+            <td><b>Sobota 7&nbsp;tygodnia wielkanocnego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, opiekę NMP, potrzebne łaski dla Asi
+    z&nbsp;okazji imienin<br>
+2. + Alojzego Ciszkiewicza
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + ks.&nbsp;Stanisława Chmielewskiego<br>
+2. + Zofię, Stanisława Żyłów, zmarłych z&nbsp;rodziny Radom
+    i&nbsp;Biernackich
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>NIEDZIELA, 24&nbsp;maja</b></td>
+            <td><b>
+                <span style="color: red">Uroczystość Zesłania Ducha Świętego</span>
+            </b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">7.30</td>
+            <td>
++ Zofię, Wincentego, Henryka z&nbsp;rodziny Wyrzykowskich, Bogusława
+    Stępińskiego
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.30</td>
+            <td>
+W&nbsp;intencji Parafian
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">11.15</td>
+            <td>
++ Jana, Janinę, Ryszarda Jońców
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">12.30</td>
+            <td>
++ Kazimierza Grządkowskiego (13&nbsp;rocz. śm.), ks.&nbsp;Kazimierza
+    Jarochę (13&nbsp;rocz. śm.)
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Pawła Grzesiaka<br>
+2. + Stanisława Kuśmierza, dusze w&nbsp;czyśćcu cierpiące
+            </td>
+        </tr>
+    </tbody>
+</table>
+    </div>
+</div>
+<br>
+
+<div class="card">
+    <div class="card-header">
+        <h3>17.&nbsp;maja 2026&nbsp;r.</h3>
+        <h3>Uroczystość Wniebowstąpienia Pańskiego</h3>
+    </div>
+    <div class="col">
+        <p>1. Przed nami ostatni tydzień okresu wielkanocnego. Zapraszamy
+        na&nbsp;nowennę przed Uroczystością Zesłania Ducha Świętego,
+        codziennie podczas nabożeństwa majowego o&nbsp;godz.&nbsp;17.30.</p>
+        <p>2. Spotkanie dla dzieci pierwszokomunijnych w&nbsp;środę
+        o&nbsp;godz.&nbsp;18.30 i&nbsp;w&nbsp;sobotę o&nbsp;godz.&nbsp;9.00.</p>
+        <p>3. Spotkanie dla dzieci z&nbsp;klas czwartych obchodzących rocznicę
+        komunii świętej w&nbsp;sobotę 23&nbsp;maja o&nbsp;godz.&nbsp;10.30.</p>
+        <p>4. Zapraszamy młodzież z&nbsp;klas VI i&nbsp;VII przygotowujących
+        się do&nbsp;sakramentu bierzmowania w&nbsp;przyszłych latach,
+        na&nbsp;ostatnie w&nbsp;tym roku spotkanie formacyjne, w&nbsp;sobotę
+        od&nbsp;godz.&nbsp;9.00 do&nbsp;11.00 w&nbsp;salce na&nbsp;plebanii.</p>
+        <p>5. W&nbsp;najbliższą sobotę, w&nbsp;Wigilię Zesłania Ducha Świętego,
+        zapraszamy na&nbsp;kolejny wieczór uwielbienia. Początek
+        o&nbsp;godz.&nbsp;18.00.</p>
+        <p>6. W&nbsp;najbliższy wtorek, 19&nbsp;maja, jak w&nbsp;każdy trzeci
+        wtorek miesiąca w&nbsp;tym roku, zapraszamy na&nbsp;kolejne spotkanie
+        Akademii Biblijnej w&nbsp;Sandomierzu; poprowadzi je&nbsp;wykładowca
+        Pisma świętego w&nbsp;naszym seminarium, ks.&nbsp;Paweł Lasek. Tematem
+        tego spotkania będzie: List św.&nbsp;Pawła do&nbsp;Rzymian. Spotkanie
+        będzie we&nbsp;wtorek, 19&nbsp;maja, o&nbsp;godz.&nbsp;18.45,
+        w&nbsp;auli Wyższego Seminarium Duchownego. W&nbsp;spotkaniach będzie
+        można uczestniczyć także internetowo, będą ono transmitowane przez
+        Facebooka, na&nbsp;profilu <em><a href="https://www.facebook.com/wsdsandomierz/">WSD Sandomierz</a> </em>.
+        Serdecznie zapraszamy!</p>
+        <p>7. W&nbsp;najbliższą sobotę, 23&nbsp;maja, odbędzie się wydarzenie
+        upamiętniające biskupa Franciszka Jopa, kapłana i&nbsp;sufragana
+        sandomierskiego. O&nbsp;godz.&nbsp;9.30 konferencja naukowa
+        w&nbsp;Diecezjalnym Centrum <em>Quo Vadis</em>. O&nbsp;17.30
+        w&nbsp;bazylice katedralnej Msza Święta z&nbsp;okazji 50&nbsp;rocznicy
+        śmierci bpa Jopa.</p>
+        <p>8. W&nbsp;przyszłą niedzielę, 24&nbsp;maja, o&nbsp;godz.&nbsp;16.30
+        w&nbsp;bazylice katedralnej odbędzie się koncert pieśni maryjnych
+        w&nbsp;wykonaniu chóru katedralnego.</p>
+        <p>9. W&nbsp;ostatnim czasie swoją ziemską pielgrzymkę zakończył nasz
+        parafianin Henryk Piętowski. Polećmy go&nbsp;Bożemu miłosierdziu.</p>
+        <p>10. Dziękujemy za&nbsp;wszelkie duchowe i&nbsp;materialne wsparcie
+        naszej parafii. Wszystkim obchodzącym imieniny, rocznice
+        i&nbsp;jubileusze składamy serdeczne życzenia. Naszym parafianom
+        i&nbsp;gościom życzymy błogosławionej niedzieli.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 17.&nbsp;maja 2026&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Rafał Kobiałka</span>
+        </p>
+    </div>
+</div>
+<br>
+
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
--- intentions.php
+++ intentions.php
@@ -1,177 +1,180 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            18.05 – 24.05.2026&nbsp;r.
+            25.05 – 31.05.2026&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="aktualnosci_day">
-            <td><b>PONIEDZIAŁEK, 18&nbsp;maja</b></td>
-            <td><b>Poniedziałek 7&nbsp;tygodnia wielkanocnego</b></td>
+            <td><b>PONIEDZIAŁEK, 25&nbsp;maja</b></td>
+            <td><b>Święto NMP Matki Kościoła</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Marka Ziębę<br>
-2. O&nbsp;szczęśliwą operację i&nbsp;zdrowie dla Agnieszki
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>
-1. + ks.&nbsp;Stanisława Chmielewskiego<br>
-2. + Krzysztofa (rocz.), Elżbietę i&nbsp;Kazimierza Bajdów
-            </td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>WTOREK, 19&nbsp;maja</b></td>
-            <td><b>Wtorek 7&nbsp;tygodnia wielkanocnego</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>
-1. + Zofię Skorupską<br>
-2. + Mieczysława Frydla
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>
-1. O&nbsp;Boże błogosławieństwo, opiekę NMP, potrzebne łaski dla
-    Rozalii i&nbsp;Antosia w&nbsp;4&nbsp;rocznicę urodzin<br>
-2. + ks.&nbsp;Stanisława Chmielewskiego
-            </td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>ŚRODA, 20&nbsp;maja</b></td>
-            <td><b>Środa 7&nbsp;tygodnia wielkanocnego</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>
-1. O&nbsp;Boże błogosławieństwo dla księży Łukasza i&nbsp;Szczepana
-    w&nbsp;rocznicę święceń<br>
-2. + Mariana Cąbrowskiego
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>
-1. + ks.&nbsp;Stanisława Chmielewskiego<br>
-2. + Zofię, Stanisława Kilian i&nbsp;zmarłych z&nbsp;rodziny<br>
-3. + Adama Sochę
-            </td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>CZWARTEK, 21&nbsp;maja</b></td>
-            <td><b>Czwartek 7&nbsp;tygodnia wielkanocnego</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>
-1. + Marka Steca<br>
+1. + Mariana Biało (2&nbsp;rocz. śm.)<br>
 2. + Kazimierza Woźniaka
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Ewę Marciniak<br>
-2. + Wacława Boryckiego<br>
-3. + Adama Sochę<br>
-4. + Andrzeja Wosia
+1. + Stefanię i&nbsp;Ludwika Karoniów<br>
+2. + ks.&nbsp;Stanisława Chmielewskiego<br>
+3. + Andrzeja Wosia
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>PIĄTEK, 22&nbsp;maja</b></td>
-            <td><b>Piątek 7&nbsp;tygodnia wielkanocnego</b></td>
+            <td><b>WTOREK, 26&nbsp;maja</b></td>
+            <td><b>Wspomnienie św. Filipa Neri, Dzień Matki</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Kazimierza Woźniaka<br>
-2. + Seweryna Rybę, Tadeusza Mazura i&nbsp;Witolda Rybę (8&nbsp;rocz. śm.)
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">18.00</td>
-            <td>
-1. + Jana Gajdę<br>
-2. + Krystynę Kuśmirę<br>
-3. + Adama Sochę
-            </td>
-        </tr>
-
-
-        <tr class="aktualnosci_day">
-            <td><b>SOBOTA, 23&nbsp;maja</b></td>
-            <td><b>Sobota 7&nbsp;tygodnia wielkanocnego</b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>
-1. O&nbsp;Boże błogosławieństwo, opiekę NMP, potrzebne łaski dla Asi
-    z&nbsp;okazji imienin<br>
-2. + Alojzego Ciszkiewicza
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla
+    księdza Damiana w&nbsp;rocznicę święceń<br>
+2. + Mateusza Frańczaka
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
 1. + ks.&nbsp;Stanisława Chmielewskiego<br>
-2. + Zofię, Stanisława Żyłów, zmarłych z&nbsp;rodziny Radom
-    i&nbsp;Biernackich
+2. + Mariannę, Stefana, Krzysztofa Kwietniów i&nbsp;zmarłych
+    z&nbsp;rodzin Kwietniów, Zybałów, Wiśniewskich i&nbsp;Misiudów
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>NIEDZIELA, 24&nbsp;maja</b></td>
-            <td><b>
-                <span style="color: red">Uroczystość Zesłania Ducha Świętego</span>
-            </b></td>
+            <td><b>ŚRODA, 27&nbsp;maja</b></td>
+            <td><b>Środa 8&nbsp;tygodnia zwykłego</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
-            <td class="center">7.30</td>
+            <td class="center">6.30</td>
             <td>
-+ Zofię, Wincentego, Henryka z&nbsp;rodziny Wyrzykowskich, Bogusława
-    Stępińskiego
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">9.30</td>
-            <td>
-W&nbsp;intencji Parafian
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">11.15</td>
-            <td>
-+ Jana, Janinę, Ryszarda Jońców
-            </td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">12.30</td>
-            <td>
-+ Kazimierza Grządkowskiego (13&nbsp;rocz. śm.), ks.&nbsp;Kazimierza
-    Jarochę (13&nbsp;rocz. śm.)
+1. + Albinę Borkowską<br>
+2. + Mariana Cąbrowskiego
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Pawła Grzesiaka<br>
-2. + Stanisława Kuśmierza, dusze w&nbsp;czyśćcu cierpiące
+1. + Stanisława Judę (2&nbsp;rocz. śm.)<br>
+2. + Marka Grębowca
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>CZWARTEK, 28&nbsp;maja</b></td>
+            <td><b>Święto Jezusa Chrystusa Najwyższego i&nbsp;Wiecznego
+            Kapłana</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Jana Gajdę<br>
+2. + Marka Ziębę
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Agnieszkę Pietrusińską (3&nbsp;rocz. śm.)<br>
+2. + Stanisława i Elżbietę Komorowskich
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>PIĄTEK, 29&nbsp;maja</b></td>
+            <td><b>Wspomnienie św.&nbsp;Urszuli Ledóchowskiej</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Teodozję i&nbsp;Zdzisława Dudków, Stanisławę i&nbsp;Mariana
+    Mądrych, zmarłych z&nbsp;rodziny i&nbsp;dusze w&nbsp;czyśćcu
+    cierpiące<br>
+2. + Kazimierza Woźniaka
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + ks.&nbsp;Stanisława Chmielewskiego<br>
+2. + Ryszarda Kubickiego (18&nbsp;rocz. śm.), Stanisława
+    Niewiadomskiego<br>
+3. + Karola Malca
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>SOBOTA, 30&nbsp;maja</b></td>
+            <td><b>Sobota 8&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla
+    Michaliny<br>
+2. + Stanisława Łukawskiego
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + ks.&nbsp;Stanisława Chmielewskiego<br>
+2. + Adama, Stefana Osuchów, Kazimierę, Stanisława, Sławomira
+    Rykowskich<br>
+3. + Karola Malca
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>NIEDZIELA, 31&nbsp;maja</b></td>
+            <td><b>
+                <span style="color: red">Niedziela Trójcy Świętej</span>
+            </b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">7.30</td>
+            <td>
++ Mariannę Brzyszcz
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.30</td>
+            <td>
++ Anielę, Romualda, Pamelę, Aleksandra Kapisów
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">11.15</td>
+            <td>
++ Mariannę i&nbsp;Witolda
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">12.30</td>
+            <td>
+1. W&nbsp;intencji Parafian<br>
+2. W&nbsp;intencji młodzieży, która przyjęła w&nbsp;tym roku
+    sakrament bierzmowania
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Stanisława Sapielaka<br>
+2. + Annę Gach
             </td>
         </tr>
     </tbody>
