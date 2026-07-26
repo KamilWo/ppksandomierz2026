03763b4f91debf7bbfcd7eead92406f7fc4d25a1
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2026-07-26 16:11:16 +0200

    2026-07-26

--- archiwum15.php
+++ archiwum15.php
@@ -19,7 +19,245 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2026 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 12.&nbsp;lipca 2026&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 26.&nbsp;lipca 2026&nbsp;r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+            20.07 – 26.07.2026&nbsp;r.
+        </h3>
+    </div>
+    <div class="col">
+<table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+    <tbody>
+        <tr class="aktualnosci_day">
+            <td><b>PONIEDZIAŁEK, 20&nbsp;lipca</b></td>
+            <td><b>Poniedziałek 16&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Andrzeja Skoczylasa (4&nbsp;rocz. śm.)
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Zofię i&nbsp;Czesława Chlebnych<br>
+2. + Monikę Bernaś<br>
+3. + Marka Ziębę <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>WTOREK, 21&nbsp;lipca</b></td>
+            <td><b>Wtorek 16&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Stanisława Muchę
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla
+    Joanny i&nbsp;Michała w&nbsp;9&nbsp;rocznice ślubu<br>
+2. + Monikę Bernaś<br>
+3. + Gustawa Kaniosa <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>ŚRODA, 22&nbsp;lipca</b></td>
+            <td><b>Święto św.&nbsp;Marii Magdaleny</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Urszulę, Krzysztofa i&nbsp;zmarłych z&nbsp;ich rodzin oraz dusze
+    w&nbsp;czyśćcu cierpiące
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Annę Błażuk (rocz.)<br>
+2. + Monikę Bernaś<br>
+3. + Tomasza Nowaka <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>CZWARTEK, 23&nbsp;lipca</b></td>
+            <td><b>Święto św.&nbsp;Brygidy, patronki Europy</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Andrzeja Karelusa
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Edmunda, Leopolda Szafrańskiego (6&nbsp;rocz. śm.), Teofilę
+    i&nbsp;Walentego Adamców, Helenę i&nbsp;Jana Szafrańskich<br>
+2. + Czesława Zeliasia<br>
+3. + Barbarę Chorab <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>PIĄTEK, 24&nbsp;lipca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Kingi</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Kazimierza Woźniaka
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Marię, Władysława, Czesława Barańskich, Władysławę
+    i&nbsp;Andrzeja Krawczyków<br>
+2. + Tomasza Nowaka<br>
+3. + Karola Malca <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>SOBOTA, 25&nbsp;lipca</b></td>
+            <td><b>Święto św.&nbsp;Jakuba Apostoła, wspomnienie
+                św.&nbsp;Krzysztofa<br>
+            Dzień imienin księdza Biskupa Ordynariusza</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla Ani
+    z&nbsp;okazji imienin
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. W&nbsp;intencji Parafian<br>
+2. ++ Za&nbsp;zmarłych z&nbsp;rodzin Kurków, Sowizdrzałów, Henkelmanów,
+    Pasiów, Piegońskich oraz dusze w&nbsp;czyśćcu cierpiące<br>
+3. + Adama Sochę <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>NIEDZIELA, 26&nbsp;lipca</b></td>
+            <td><b>
+                <span style="color: red">XVII&nbsp;niedziela zwykła,
+                Rok&nbsp;A</span>
+            </b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">7.30</td>
+            <td>
++ Małgorzatę Rak (22&nbsp;rocz. śm.), zmarłych z&nbsp;rodzin Raków
+    i&nbsp;Mazurów
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.30</td>
+            <td>
+Dziękczynna z&nbsp;prośbą o&nbsp;Boże błogosławieństwo, potrzebne
+    łaski, opiekę NMP dla Ewy i&nbsp;Jerzego z&nbsp;okazji
+    50&nbsp;rocznicy ślubu
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">11.15</td>
+            <td>
++ Bogusława Sarzyńskiego, Annę, Janusza, Stanisława Rożmiejów, Zofię,
+    Mikołaja, Eugeniusza Kapustów
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">12.30</td>
+            <td>
++ Mariannę, Jakuba, Mieczysława Pijewskich
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla Dawida<br>
+2. + Annę Gach
+            </td>
+        </tr>
+    </tbody>
+</table>
+    </div>
+</div>
+<br>
+
+<div class="card">
+    <div class="card-header">
+        <h3>19.&nbsp;lipca 2026&nbsp;r.</h3>
+        <h3>XVI&nbsp;niedziela zwykła, Rok&nbsp;A</h3>
+    </div>
+    <div class="col">
+        <p>1. W&nbsp;tym tygodniu oddajemy cześć świętym: w&nbsp;środę świętej
+        Marii Magdalenie, w&nbsp;czwartek świętej Brygidzie, w&nbsp;piątek
+        świętej Kindze i&nbsp;w&nbsp;sobotę świętemu Jakubowi i&nbsp;świętemu
+        Krzysztofowi.</p>
+        <p>2. W&nbsp;przyszłą sobotą, 25&nbsp;lipca, swoje imieniny obchodzi
+        ksiądz biskup Krzysztof Nitkiewicz, ordynariusz naszej diecezji.
+        Pamiętajmy w&nbsp;modlitwie o&nbsp;naszym Pasterzu.</p>
+        <p>3. W&nbsp;sobotę po&nbsp;Mszy Świętej wieczornej poświęcimy pojazdy
+        mechaniczne.</p>
+        <p>4. W&nbsp;przyszłą niedzielę będzie możliwość wesprzeć materialnie
+        chorą Wiktorię Bednarz. Wiktoria przeszła już operacje głowy, serca
+        i&nbsp;nóg. Czeka ją&nbsp;operacja kręgosłupa i&nbsp;czaszki, bez
+        której dojdzie do&nbsp;ucisku i&nbsp;uszkodzeń w&nbsp;mózgu. Wiktoria
+        wymaga intensywnej rehabilitacji oraz specjalistycznych terapii koszty
+        w&nbsp;skali roku są&nbsp;ogromne. O&nbsp;możliwość zbiórki zwrócili
+        się do&nbsp;nas rodzice. Otrzymaliśmy także pismo z&nbsp;Kurii
+        Diecezjalnej potwierdzające autentyczność prośby. Zachęcamy,
+        w&nbsp;miarę możliwości do&nbsp;wsparcia.</p>
+        <p>5. W&nbsp;dniach od&nbsp;29&nbsp;lipca do&nbsp;8&nbsp;sierpnia 2027
+        roku odbędą Światowe Dni Młodzieży w&nbsp;Korei Południowej. Koszt
+        wyjazdu to&nbsp;ok.&nbsp;10&nbsp;000&nbsp;zł. Szczegółowe informacje
+        i&nbsp;zapisy u&nbsp;ks.&nbsp;Krystiana.</p>
+        <p>6. W&nbsp;przyszłym roku planujemy dwie pielgrzymki. W&nbsp;dniach
+        od&nbsp;30&nbsp;kwietnia do&nbsp;3&nbsp;maja odbędzie się pielgrzymka
+        śladami Bożego Miłosierdzia do&nbsp;Wilna, jako wotum wdzięczności
+        za&nbsp;czas peregrynacji obrazu Jezusa Miłosiernego w&nbsp;naszej
+        parafii. Z&nbsp;kolei w&nbsp;drugiej połowie sierpnia odbędzie się
+        pielgrzymka na&nbsp;Cypr śladami św.&nbsp;Pawła. Szczegółowe
+        informacje podamy we&nbsp;wrześniu.</p>
+        <p>7. Bardzo dziękujemy raz jeszcze Panom Wacławowi i&nbsp;Leszkowi
+        za&nbsp;konserwację i&nbsp;odnowienie drzwi wejściowych
+        do&nbsp;naszego kościoła.</p>
+        <p>8. W&nbsp;ostatnim czasie swoją ziemską pielgrzymkę zakończyli nasi
+        parafianie: Sławomir Litwiński, Kazimiera Sadaj, Julian Dragan,
+        Aleksandra Kowalska, Alfred Czech. Polećmy ich Bożemu miłosierdziu.</p>
+        <p>9. Dziękujemy za&nbsp;każde duchowe i&nbsp;materialne wsparcie
+        parafii. Wszystkim obchodzącym imieniny, rocznice i&nbsp;jubileusze
+        składamy serdeczne życzenia. Naszym Parafianom i&nbsp;Gościom życzymy
+        błogosławionej niedzieli i&nbsp;spokojnych wakacji.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 19.&nbsp;lipca 2026&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Rafał Kobiałka</span>
+        </p>
+    </div>
+</div>
+<br>
+
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
--- posts.php
+++ posts.php
@@ -1,50 +1,31 @@
 <div class="card">
     <div class="card-header">
-        <h3>19.&nbsp;lipca 2026&nbsp;r.</h3>
-        <h3>XVI&nbsp;niedziela zwykła, Rok&nbsp;A</h3>
+        <h3>26.&nbsp;lipca 2026&nbsp;r.</h3>
+        <h3>XVII&nbsp;niedziela zwykła, Rok&nbsp;A</h3>
     </div>
     <div class="col">
-        <p>1. W&nbsp;tym tygodniu oddajemy cześć świętym: w&nbsp;środę świętej
-        Marii Magdalenie, w&nbsp;czwartek świętej Brygidzie, w&nbsp;piątek
-        świętej Kindze i&nbsp;w&nbsp;sobotę świętemu Jakubowi i&nbsp;świętemu
-        Krzysztofowi.</p>
-        <p>2. W&nbsp;przyszłą sobotą, 25&nbsp;lipca, swoje imieniny obchodzi
-        ksiądz biskup Krzysztof Nitkiewicz, ordynariusz naszej diecezji.
-        Pamiętajmy w&nbsp;modlitwie o&nbsp;naszym Pasterzu.</p>
-        <p>3. W&nbsp;sobotę po&nbsp;Mszy Świętej wieczornej poświęcimy pojazdy
-        mechaniczne.</p>
-        <p>4. W&nbsp;przyszłą niedzielę będzie możliwość wesprzeć materialnie
-        chorą Wiktorię Bednarz. Wiktoria przeszła już operacje głowy, serca
-        i&nbsp;nóg. Czeka ją&nbsp;operacja kręgosłupa i&nbsp;czaszki, bez
-        której dojdzie do&nbsp;ucisku i&nbsp;uszkodzeń w&nbsp;mózgu. Wiktoria
-        wymaga intensywnej rehabilitacji oraz specjalistycznych terapii koszty
-        w&nbsp;skali roku są&nbsp;ogromne. O&nbsp;możliwość zbiórki zwrócili
-        się do&nbsp;nas rodzice. Otrzymaliśmy także pismo z&nbsp;Kurii
-        Diecezjalnej potwierdzające autentyczność prośby. Zachęcamy,
-        w&nbsp;miarę możliwości do&nbsp;wsparcia.</p>
-        <p>5. W&nbsp;dniach od&nbsp;29&nbsp;lipca do&nbsp;8&nbsp;sierpnia 2027
-        roku odbędą Światowe Dni Młodzieży w&nbsp;Korei Południowej. Koszt
-        wyjazdu to&nbsp;ok.&nbsp;10&nbsp;000&nbsp;zł. Szczegółowe informacje
-        i&nbsp;zapisy u&nbsp;ks.&nbsp;Krystiana.</p>
-        <p>6. W&nbsp;przyszłym roku planujemy dwie pielgrzymki. W&nbsp;dniach
-        od&nbsp;30&nbsp;kwietnia do&nbsp;3&nbsp;maja odbędzie się pielgrzymka
-        śladami Bożego Miłosierdzia do&nbsp;Wilna, jako wotum wdzięczności
-        za&nbsp;czas peregrynacji obrazu Jezusa Miłosiernego w&nbsp;naszej
-        parafii. Z&nbsp;kolei w&nbsp;drugiej połowie sierpnia odbędzie się
-        pielgrzymka na&nbsp;Cypr śladami św.&nbsp;Pawła. Szczegółowe
-        informacje podamy we&nbsp;wrześniu.</p>
-        <p>7. Bardzo dziękujemy raz jeszcze Panom Wacławowi i&nbsp;Leszkowi
-        za&nbsp;konserwację i&nbsp;odnowienie drzwi wejściowych
-        do&nbsp;naszego kościoła.</p>
-        <p>8. W&nbsp;ostatnim czasie swoją ziemską pielgrzymkę zakończyli nasi
-        parafianie: Sławomir Litwiński, Kazimiera Sadaj, Julian Dragan,
-        Aleksandra Kowalska, Alfred Czech. Polećmy ich Bożemu miłosierdziu.</p>
-        <p>9. Dziękujemy za&nbsp;każde duchowe i&nbsp;materialne wsparcie
+        <p>1. W&nbsp;tym tygodniu oddajemy cześć świętym: w&nbsp;środę
+        rodzeństwu z&nbsp;Betanii – Marcie, Marii i&nbsp;Łazarzowi,
+        w&nbsp;piątek św.&nbsp;Ignacemu z&nbsp;Loyoli, w&nbsp;sobotę
+        św.&nbsp;Alfonsowi de&nbsp;Liguori.</p>
+        <p>2. W&nbsp;przyszły wtorek, 4&nbsp;sierpnia, wyruszy
+        z&nbsp;Sandomierza piesza pielgrzymka na&nbsp;Jasną Górę. Serdecznie
+        zachęcamy do&nbsp;udziału. Szczegóły na&nbsp;plakacie
+        w&nbsp;gablocie.</p>
+        <p>3. Do&nbsp;18&nbsp;września trwa rekrutacja do&nbsp;Wyższego
+        Seminarium Duchownego.</p>
+        <p>4. Wydział Duszpasterstwa Rodzin serdecznie zaprasza
+        do&nbsp;podjęcia nauki w&nbsp;Diecezjalnym Studium Życia
+        i&nbsp;Rodziny. Szczegółowe informacje na&nbsp;plakacie
+        w&nbsp;gablocie.</p>
+        <p>5. Po&nbsp;Mszy Świętej można wesprzeć ofiarą leczenie chorej
+        Wiktorii Bednarz.</p>
+        <p>6. Dziękujemy za&nbsp;wszelkie duchowe i&nbsp;materialne wsparcie
         parafii. Wszystkim obchodzącym imieniny, rocznice i&nbsp;jubileusze
-        składamy serdeczne życzenia. Naszym Parafianom i&nbsp;Gościom życzymy
-        błogosławionej niedzieli i&nbsp;spokojnych wakacji.</p>
+        składamy serdeczne życzenia. Wszystkim Parafianom i&nbsp;Gościom
+        życzymy błogosławionej niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 19.&nbsp;lipca 2026&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 26.&nbsp;lipca 2026&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Rafał Kobiałka</span>
         </p>
     </div>
