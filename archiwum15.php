c83f303bcb51e69e3f31e9343f9cc2b555319997
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2026-08-03 15:46:02 +0200

    2026-08-02

--- archiwum15.php
+++ archiwum15.php
@@ -19,7 +19,219 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2026 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 26.&nbsp;lipca 2026&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 2.&nbsp;sierpnia 2026&nbsp;r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+            27.07 – 2.08.2026&nbsp;r.
+        </h3>
+    </div>
+    <div class="col">
+<table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+    <tbody>
+        <tr class="aktualnosci_day">
+            <td><b>PONIEDZIAŁEK, 27&nbsp;lipca</b></td>
+            <td><b>Poniedziałek 17&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Stefanię Seweryńską (12&nbsp;rocz. śm.)
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Krzysztofa, Stefanię, Józefa  Łukawskich, Antoninę Majcher<br>
+2. + Mariannę Chmiel <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>WTOREK, 28&nbsp;lipca</b></td>
+            <td><b>Wtorek 17&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Seweryna Polewskiego
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Wacława Boryckiego<br>
+2. + Jana Jakułę<br>
+3. + Józefa Stanka <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>ŚRODA, 29&nbsp;lipca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Marty, Marii
+            i&nbsp;Łazarza</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + W&nbsp;intencji zmarłych z&nbsp;rodzin Steców i&nbsp;Błajszczaków
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Mateusza Frańczaka<br>
+2. + Andrzeja Wosia <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>CZWARTEK, 30&nbsp;lipca</b></td>
+            <td><b>Czwartek 17&nbsp;tygodnia zwykłego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Zofię, Wincentego Kołodziej, Marię, Kazimierza Włoch, Juliannę,
+    Walentego Bulirów, Irenę, Tadeusza Kołacz, Stanisława, Stanisławę Woś
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Stefana, Genowefę, Jarosława, Annę, Józefa Burków, Paulinę,
+    Mieczysława Ramus, Janinę, Stefana Placha, Kazimierza Pietrasa,
+    Marię Potocką, Józefę i&nbsp;Romana Siudak<br>
+2. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla Neli
+    Biernackiej i&nbsp;jej rodziców<br>
+3. + Annę Gach <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>PIĄTEK, 31&nbsp;lipca</b></td>
+            <td><b>Wspomnienie św.&nbsp;Ignacego Loyoli</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. + Zofię Kuminek
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. + Małgorzatę (9&nbsp;rocz. śm.) i&nbsp;Waldemara Walczynów<br>
+2. + Małgorzatę Juda <strong>(poza parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>SOBOTA, 1&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Alfonsa Marii Liguoriego</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla
+    dzieci i&nbsp;chrześniaków
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
+1. Wynagradzająca za&nbsp;grzechy popełnione przeciw Niepokalanemu
+    Sercu Maryi<br>
+2. + Mariannę i&nbsp;Eugeniusza Jedlińskich<br>
+3. + Zuzannę (1&nbsp;rocz. śm.) i&nbsp;Władysława Marców <strong>(poza
+    parafią)</strong>
+            </td>
+        </tr>
+
+
+        <tr class="aktualnosci_day">
+            <td><b>NIEDZIELA, 2&nbsp;sierpnia</b></td>
+            <td><b>
+                <span style="color: red">XVIII&nbsp;niedziela zwykła,
+                Rok&nbsp;A</span>
+            </b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">7.30</td>
+            <td>
+W&nbsp;intencji Parafian
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">9.30</td>
+            <td>
++ Stanisława i&nbsp;Stanisławę Wolińskich, i&nbsp;zmarłych z&nbsp;rodziny
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">11.15</td>
+            <td>
+O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla sióstr
+    i&nbsp;braci z&nbsp;kół żywego różańca
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">12.30</td>
+            <td>
++ Tomasza Mroczkowskiego (1&nbsp;rocz. śm.)
+            </td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">18.00</td>
+            <td>
++ Jana (rocz.), Klarę, Ryszarda Chrząstowskich, Jana Makowskiego
+            </td>
+        </tr>
+    </tbody>
+</table>
+    </div>
+</div>
+<br>
+
+<div class="card">
+    <div class="card-header">
+        <h3>26.&nbsp;lipca 2026&nbsp;r.</h3>
+        <h3>XVII&nbsp;niedziela zwykła, Rok&nbsp;A</h3>
+    </div>
+    <div class="col">
+        <p>1. W&nbsp;tym tygodniu oddajemy cześć świętym: w&nbsp;środę
+        rodzeństwu z&nbsp;Betanii – Marcie, Marii i&nbsp;Łazarzowi,
+        w&nbsp;piątek św.&nbsp;Ignacemu z&nbsp;Loyoli, w&nbsp;sobotę
+        św.&nbsp;Alfonsowi de&nbsp;Liguori.</p>
+        <p>2. W&nbsp;przyszły wtorek, 4&nbsp;sierpnia, wyruszy
+        z&nbsp;Sandomierza piesza pielgrzymka na&nbsp;Jasną Górę. Serdecznie
+        zachęcamy do&nbsp;udziału. Szczegóły na&nbsp;plakacie
+        w&nbsp;gablocie.</p>
+        <p>3. Do&nbsp;18&nbsp;września trwa rekrutacja do&nbsp;Wyższego
+        Seminarium Duchownego.</p>
+        <p>4. Wydział Duszpasterstwa Rodzin serdecznie zaprasza
+        do&nbsp;podjęcia nauki w&nbsp;Diecezjalnym Studium Życia
+        i&nbsp;Rodziny. Szczegółowe informacje na&nbsp;plakacie
+        w&nbsp;gablocie.</p>
+        <p>5. Po&nbsp;Mszy Świętej można wesprzeć ofiarą leczenie chorej
+        Wiktorii Bednarz.</p>
+        <p>6. Dziękujemy za&nbsp;wszelkie duchowe i&nbsp;materialne wsparcie
+        parafii. Wszystkim obchodzącym imieniny, rocznice i&nbsp;jubileusze
+        składamy serdeczne życzenia. Wszystkim Parafianom i&nbsp;Gościom
+        życzymy błogosławionej niedzieli.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 26.&nbsp;lipca 2026&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Rafał Kobiałka</span>
+        </p>
+    </div>
+</div>
+<br>
+
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
--- intentions.php
+++ intentions.php
@@ -1,173 +1,174 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            27.07 – 2.08.2026&nbsp;r.
+            3.08 – 9.08.2026&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="aktualnosci_day">
-            <td><b>PONIEDZIAŁEK, 27&nbsp;lipca</b></td>
-            <td><b>Poniedziałek 17&nbsp;tygodnia zwykłego</b></td>
+            <td><b>PONIEDZIAŁEK, 3&nbsp;sierpnia</b></td>
+            <td><b>Poniedziałek 18&nbsp;tygodnia zwykłego</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Stefanię Seweryńską (12&nbsp;rocz. śm.)
++ Aleksandrę, Józefa, Marię, Mariana Gradzińskich, zmarłych z&nbsp;rodzin Żubrynowiczów i Chałupczaków
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Krzysztofa, Stefanię, Józefa  Łukawskich, Antoninę Majcher<br>
-2. + Mariannę Chmiel <strong>(poza parafią)</strong>
+1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla Anny
+    z&nbsp;okazji urodzin<br>
+2. Dziękczynna z&nbsp;prośbą o&nbsp;Boże błogosławieństwo, potrzebne
+    łaski, opiekę NMP dla Macieja i&nbsp;Klaudii<br>
+3. + Alicję Bieżanek <strong>(poza parafią)</strong>
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>WTOREK, 28&nbsp;lipca</b></td>
-            <td><b>Wtorek 17&nbsp;tygodnia zwykłego</b></td>
+            <td><b>WTOREK, 4&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Jana Marii Vianneya</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Seweryna Polewskiego
++ Krystynę Kuśmirę
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Wacława Boryckiego<br>
-2. + Jana Jakułę<br>
-3. + Józefa Stanka <strong>(poza parafią)</strong>
+1. Zofię i&nbsp;Władysława Szczudło, Mariana Czerepaka, Jana
+    Kędzierskiego<br>
+2. + Henryka Piętowskiego <strong>(poza parafią)</strong>
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>ŚRODA, 29&nbsp;lipca</b></td>
-            <td><b>Wspomnienie św.&nbsp;Marty, Marii
-            i&nbsp;Łazarza</b></td>
+            <td><b>ŚRODA, 5&nbsp;sierpnia</b></td>
+            <td><b>Środa 18&nbsp;tygodnia zwykłego</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + W&nbsp;intencji zmarłych z&nbsp;rodzin Steców i&nbsp;Błajszczaków
++ Wiesławę Wałcerz (rocz. śm.), Stefanię, Franciszka Kowalskich, Marię
+    Woś, Celinę, Jana, Jarosława Gucałów
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Mateusza Frańczaka<br>
-2. + Andrzeja Wosia <strong>(poza parafią)</strong>
+1. + Julię i&nbsp;Antoniego Serafinów<br>
+2. + Anastazję Grembowiec<br>
+3. + Mariannę Chmiel <strong>(poza parafią)</strong>
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>CZWARTEK, 30&nbsp;lipca</b></td>
-            <td><b>Czwartek 17&nbsp;tygodnia zwykłego</b></td>
+            <td><b>CZWARTEK, 6&nbsp;sierpnia</b></td>
+            <td><b>Święto Przemienienia Pańskiego</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Zofię, Wincentego Kołodziej, Marię, Kazimierza Włoch, Juliannę,
-    Walentego Bulirów, Irenę, Tadeusza Kołacz, Stanisława, Stanisławę Woś
+O&nbsp;świętość kapłanów i&nbsp;o&nbsp;nowe powołania
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Stefana, Genowefę, Jarosława, Annę, Józefa Burków, Paulinę,
-    Mieczysława Ramus, Janinę, Stefana Placha, Kazimierza Pietrasa,
-    Marię Potocką, Józefę i&nbsp;Romana Siudak<br>
-2. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla Neli
-    Biernackiej i&nbsp;jej rodziców<br>
-3. + Annę Gach <strong>(poza parafią)</strong>
+1. + Henryka Pietruszkę<br>
+2. Dziękczynna z&nbsp;prośbą o&nbsp;Boże błogosławieństwo, potrzebne
+    łaski, opiekę NMP dla Andrzeja z&nbsp;okazji 28&nbsp;rocznicy urodzin
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>PIĄTEK, 31&nbsp;lipca</b></td>
-            <td><b>Wspomnienie św.&nbsp;Ignacego Loyoli</b></td>
+            <td><b>PIĄTEK, 7&nbsp;sierpnia</b></td>
+            <td><b>Pierwszy piątek miesiąca</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. + Zofię Kuminek
+O&nbsp;łaskę dobrej śmierci
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. + Małgorzatę (9&nbsp;rocz. śm.) i&nbsp;Waldemara Walczynów<br>
-2. + Małgorzatę Juda <strong>(poza parafią)</strong>
+1. + Andrzeja Spadło (rocz.)<br>
+2. + Weronikę Błaszczyk (8&nbsp;rocz. śm.)<br>
+3. + Józefa Wronę <strong>(poza parafią)</strong>
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>SOBOTA, 1&nbsp;sierpnia</b></td>
-            <td><b>Wspomnienie św.&nbsp;Alfonsa Marii Liguoriego</b></td>
+            <td><b>SOBOTA, 8&nbsp;sierpnia</b></td>
+            <td><b>Wspomnienie św.&nbsp;Dominika</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
             <td>
-1. O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla
-    dzieci i&nbsp;chrześniaków
++ Franciszka i&nbsp;Barbarę Misztal
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-1. Wynagradzająca za&nbsp;grzechy popełnione przeciw Niepokalanemu
-    Sercu Maryi<br>
-2. + Mariannę i&nbsp;Eugeniusza Jedlińskich<br>
-3. + Zuzannę (1&nbsp;rocz. śm.) i&nbsp;Władysława Marców <strong>(poza
-    parafią)</strong>
+1. + Marka Grębowca<br>
+2. + Jerzego Jakubczyka<br>
+3. + W&nbsp;intencji Parafian <strong>(poza parafią)</strong>
             </td>
         </tr>
 
 
         <tr class="aktualnosci_day">
-            <td><b>NIEDZIELA, 2&nbsp;sierpnia</b></td>
+            <td><b>NIEDZIELA, 9&nbsp;sierpnia</b></td>
             <td><b>
-                <span style="color: red">XVIII&nbsp;niedziela zwykła,
+                <span style="color: red">XIX&nbsp;niedziela zwykła,
                 Rok&nbsp;A</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
             <td>
-W&nbsp;intencji Parafian
++ Zofię, Wincentego, Henryka i&nbsp;zmarłych z&nbsp;rodziny
+    Wyrzykowskich oraz Bogusława Stępińskiego
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
             <td>
-+ Stanisława i&nbsp;Stanisławę Wolińskich, i&nbsp;zmarłych z&nbsp;rodziny
++ Jana, Franciszka, Bronisławę Zająców, Stanisława i&nbsp;Marka Dudków
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
             <td>
-O&nbsp;Boże błogosławieństwo, potrzebne łaski, opiekę NMP dla sióstr
-    i&nbsp;braci z&nbsp;kół żywego różańca
++ Józefę i&nbsp;Henryka Zielezińskich
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
             <td>
-+ Tomasza Mroczkowskiego (1&nbsp;rocz. śm.)
+Dziękczynna z&nbsp;prośbą o&nbsp;Boże błogosławieństwo, potrzebne
+    łaski, opiekę NMP dla Elżbiety i&nbsp;Leszka
+    w&nbsp;45&nbsp;rocznicę ślubu
             </td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
             <td>
-+ Jana (rocz.), Klarę, Ryszarda Chrząstowskich, Jana Makowskiego
+1. + Bolesławę, Stefana, Leona Podlesińskich<br>
+2. + Kazimierę Sadaj
             </td>
         </tr>
     </tbody>
